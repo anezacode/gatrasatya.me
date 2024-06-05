990f129142a676ac2e691baf1a60dab1e73521ba
AnezaCode <156332330+anezacode@users.noreply.github.com>
2024-06-05 21:04:01 +0700

    Create sasbk.java

---
+++ sasbk.java
@@ -0,0 +1,23 @@
+JAWABAN ESSAY SASBK MAPEL DPPLG
+
+bil2 = Double.parseDouble(jTextField1.getText());
+double hasil = 0;
+        
+switch (pilih) {
+        case 1 -> {
+                hasil = bil1 + bil2;
+                break;
+        } case 2 -> {
+                hasil = bil1 - bil2;
+                break;
+        } case 3 -> {
+                hasil = bil1 * bil2;
+                break;
+        } case 4 -> {
+                hasil = bil1 / bil2;
+                break;
+        }
+}
+        
+bil = "";
+jTextField1.setText(String.valueOf(hasil));
